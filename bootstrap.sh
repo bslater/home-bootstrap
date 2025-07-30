37a03b2fd360ccaae070ee7308564468612521b7
bslater <ben@theslaters.com.au>
2025-07-30 15:15:58 +1000

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Prompt for GH_TOKEN if not set
+if [[ -z "$GH_TOKEN" ]]; then
+  read -rsp "Enter your GitHub token: " GH_TOKEN
+  echo
+fi
+
+# Download the setup script
+curl -H "Authorization: token $GH_TOKEN" \
+     -L "https://raw.githubusercontent.com/bslater/home/main/setup-pi.sh" \
+     -o setup-pi.sh
+
+chmod +x setup-pi.sh
+./setup-pi.sh pihole
