6d6509675f60552eef24aac1deb71187994e6685
bslater <ben@theslaters.com.au>
2025-08-02 11:03:32 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# curl -Lso bootstrap.sh "https://raw.githubusercontent.com/bslater/home-bootstrap/main/bootstrap.sh" && chmod 700 bootstrap.sh && ./bootstrap.sh
+
 # Prompt for GH_TOKEN if not set
 if [[ -z "$GH_TOKEN" ]]; then
   read -rsp "Enter your GitHub token: " GH_TOKEN
